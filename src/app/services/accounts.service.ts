932e6fa18edfa76d1eca4104cf79651dcd3f8234
antema-andri <andrandrainaa@gmail.com>
2023-12-26 22:11:15 +0300

    commit

--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { ApiService } from './api.service';
 import { Account } from '../models/account.model';
 import { Credit } from '../models/credit.model';
@@ -13,61 +13,75 @@ const JWT_LOCALSTORE_KEY="token";
   providedIn: 'root'
 })
 export class AccountsService {
+  private api=inject(ApiService);
   static minTransact=5;
 
-  constructor(private api:ApiService) { 
+  constructor() { 
     const token = localStorage.getItem(JWT_LOCALSTORE_KEY);
     this.api.setAuthToken(''+token);
   }
 
   async getAccounts():Promise<Account[]>{
-    const accounts=await this.api.get("/accounts")
-                              .then((data)=>{
-                                return data.body as Account[];
-                              });
-    return accounts;
+    try {
+      const data=await this.api.get("/accounts");
+      return data.body as Account[];
+    } catch (error) {
+      console.error('Error fetching accounts:', error);
+      throw error;
+    }
   }
 
   async getAccountsByCustomerName(customerName:string, page:number, size:number):Promise<AccountPage>{
-    const accounts=await this.api.get("/accounts/page?name="+customerName+"&page="+page+"&size="+size)
-                              .then((data)=>{
-                                return data.body as AccountPage;
-                              });
-    return accounts;
+    try {
+      const data=await this.api.get("/accounts/page?name="+customerName+"&page="+page+"&size="+size);
+      return data.body as AccountPage;
+    } catch (error) {
+      console.error('Error fetching accountPages:', error);
+      throw error;
+    }
   }
 
   async getAccountOperations(accountId: string):Promise<AccountOperationPage>{
-    const accountOperatioPage=await this.api.get("/accounts/"+accountId+"/operationsPage")
-                              .then((data)=>{
-                                return data.body as AccountOperationPage;
-                              });
-    return accountOperatioPage;
+    try {
+      const data=await this.api.get("/accounts/"+accountId+"/operationsPage");
+      return data.body as AccountOperationPage;
+    } catch (error) {
+      console.error('Error fetching AccountOperations:', error);
+      throw error;
+    }
   }
 
   async credit(credit: Credit):Promise<Account>{
-    const account=await this.api.post("/accounts/credit", credit)
-                            .then((data)=>{
-                              alert("amount of $"+credit.amount+" successfully credeted");
-                              return data.body as Account;
-                            });
-    return account;
+    try {
+      const data=await this.api.post("/accounts/credit", credit);
+      alert("amount of $"+credit.amount+" successfully credeted");
+      return data.body as Account;
+    } catch (error) {
+      console.error('Error credit operation:', error);
+      throw error;
+    }
   }
 
   async debit(debit: Debit):Promise<Account>{
-    const account=await this.api.post("/accounts/debit", debit)
-                            .then((data)=>{
-                              alert("amount of $"+debit.amount+" successfully debited");
-                              return data.body as Account;
-                            });
-    return account;
+    try {
+      const data=await this.api.post("/accounts/debit", debit);
+      alert("amount of $"+debit.amount+" successfully debited");
+      return data.body as Account;
+    } catch (error) {
+      console.error('Error debit operation:', error);
+      throw error;
+    }
   }
 
   async transfer(transfer: Transfer):Promise<Account>{
-    const account=await this.api.post("/accounts/transfer", transfer)
-                            .then((data)=>{
-                              alert("amount of $"+transfer.amount+" successfully transferred to "+transfer.accountDestinationId);
-                              return data.body as Account;
-                            });
-    return account;
+    try {
+      const data=await this.api.post("/accounts/transfer", transfer);
+      alert("amount of $"+transfer.amount+" successfully transferred to "+transfer.accountDestinationId);
+      return data.body as Account;
+    } catch (error) {
+      console.error('Error transfert operation:', error);
+      throw error;
+    }
   }
+
 }
